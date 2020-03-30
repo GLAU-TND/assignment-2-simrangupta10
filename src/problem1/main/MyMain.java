b9fa497acf644eeb07df41d7bd13f1078d076f5b
simrangupta10 <49235060+simrangupta10@users.noreply.github.com>
2020-03-30 05:43:00 -0700

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,32 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        // taking only 7 inputs for simple demonstration with value 1,2,3,4,5,6,7
+        int i = 7;
+        while(i>0){
+            tree.add(scanner.nextInt());
+            i--;
+        }
+        /*tree looks like
+                    4
+                  /  \
+                2      6
+               / \    / \
+              1   3  5   7
+           */
+
+        //For Only Left Children 4,2,1,5 should be printed , for that we call printLeftChild()
+        //defined in MyBinarySearchTree.java
+        tree.printLeftChild();
+        //and print noLeftCounter as well
+        System.out.println(MyBinarySearchTree.noLeftCounter);
+    }
 }
